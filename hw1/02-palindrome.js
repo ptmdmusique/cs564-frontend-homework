88dd4b5a8155f834256efcae4a5ec5e500e5c026
Duc Phan <ptmdmusic@gmail.com>
2023-04-09 11:14:08 -0700

    [hw1] - finish 02

--- hw1/02-palindrome.js
+++ hw1/02-palindrome.js
@@ -1,3 +1,37 @@
-const elem = document.querySelector('input');
+const handleInput = (event) => {
+  const resultElement = document.querySelector("#result");
+  resultElement.className = ""; // Clear all classes
+  const classList = resultElement.classList;
 
-elem.addEventListener('input', handleInput);
+  const inputNumber = event.target.value;
+
+  if (parseInt(inputNumber) < 0) {
+    resultElement.textContent = "Please enter a positive number.";
+    classList.add("text-danger");
+    return;
+  }
+
+  if (inputNumber.length === 0) {
+    // Don't really care about float, we just check as usual
+    resultElement.textContent = "";
+    return;
+  }
+
+  const isNotPalindrome = inputNumber
+    .split("")
+    .some(
+      (digit, index) => digit !== inputNumber[inputNumber.length - index - 1],
+    );
+
+  if (!isNotPalindrome) {
+    resultElement.textContent = "Yes. This is a palindrome!";
+    classList.add("text-success");
+    return;
+  }
+
+  resultElement.textContent = "No. Try again.";
+  classList.add("text-danger");
+};
+
+const elem = document.querySelector("input");
+elem.addEventListener("input", handleInput);
