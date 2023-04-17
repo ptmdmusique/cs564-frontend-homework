d1f76f08686377068cb0c5c51fb1288543d236ce
Duc Phan <ptmdmusic@gmail.com>
2023-04-16 19:53:45 -0700

    [hw2] finish 03

--- hw2/03-count.js
+++ hw2/03-count.js
@@ -1,3 +1,37 @@
 // Add your code here
 
-input.addEventListener('keydown', handleKeyDown);
+const highlightClass = "bg-warning";
+
+const textContainer = document.getElementById("text-container");
+// Restructure the original text container
+const originalTextSpanList = textContainer.innerText.split(" ").map((word) => {
+  const newSpan = document.createElement("span");
+  newSpan.innerText = word;
+  newSpan.style.transitionDuration = "100ms";
+  return newSpan;
+});
+
+textContainer.innerHTML = "";
+originalTextSpanList.forEach((wordSpan) => {
+  textContainer.appendChild(wordSpan);
+  textContainer.appendChild(document.createTextNode(" "));
+});
+
+const handleHighlight = (textToHighlight) => {
+  originalTextSpanList.forEach((wordSpan) => {
+    const word = wordSpan.innerText;
+    const textToHighlightList = textToHighlight.split(" ");
+
+    wordSpan.classList[textToHighlightList.includes(word) ? "add" : "remove"](
+      highlightClass,
+    );
+  });
+};
+
+const input = document.getElementById("input");
+const handleKeyDown = (event) => {
+  const text = event.target.value;
+  handleHighlight(text);
+};
+
+input.addEventListener("input", handleKeyDown);
